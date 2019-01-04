c90a7ba723ea18ac2d371824e7c5c31ba49c24b8
Amol Deshmukh <amold86@gmail.com>
2019-01-04 13:10:44 +0530

    Add a node in linked list

---
+++ src/com/deshmukh/linkedlist/InsertNodeInLisnkedList.java
@@ -0,0 +1,30 @@
+package com.deshmukh.linkedlist;
+
+/**
+ * @author Amol Deshmukh
+ * @Created 04/01/19
+ */
+public class InsertNodeInLisnkedList {
+    public void insertAtStart(Node head, int data) {
+
+    }
+
+    public void insertAtLast(Node lastNode, int data) {
+
+    }
+
+    public void insertAfter(Node node, int data) {
+
+    }
+
+
+    public static class Node {
+
+        int data;
+        SampleLinkedList.LinkedListNode next;
+
+        Node(int d) {
+            this.data = d;
+        }
+    }
+}
