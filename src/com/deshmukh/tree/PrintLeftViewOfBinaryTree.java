1755f9a18a4ba3394c53a6c38089fbde7902cfd3
Amol Deshmukh <amold86@gmail.com>
2019-01-08 20:15:50 +0530

    verify answer

--- src/com/deshmukh/tree/PrintLeftViewOfBinaryTree.java
+++ src/com/deshmukh/tree/PrintLeftViewOfBinaryTree.java
@@ -1,16 +1,13 @@
 package com.deshmukh.tree;
 
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
  * @author Amol Deshmukh
- * @Created 07/01/19
+ * @Created 08/01/19
  */
-public class PrintRightViewOfBinaryTree {
-
-
+public class PrintLeftViewOfBinaryTree {
     static class Node {
         int data;
         Node right, left;
@@ -21,7 +18,6 @@ public class PrintRightViewOfBinaryTree {
     }
 
     public static void main(String[] args) {
-
         Node root = new Node(10);
         root.left = new Node(2);
         root.right = new Node(3);
@@ -30,41 +26,43 @@ public class PrintRightViewOfBinaryTree {
         root.right.right = new Node(15);
         root.right.left = new Node(12);
         root.right.right.left = new Node(14);
-        printRightView(root);
+        printLeftView(root);
 
         // answer is
         //10
-        //3
-        //15
+        //2
+        //7
         //14
 
-
+        //TODO check answer once
     }
 
-    private static void printRightView(Node root) {
+
+    private static void printLeftView(Node root) {
+
         if (root == null) {
             return;
         }
 
-
         Queue<Node> queue = new LinkedList<>();
+
         queue.offer(root);
+
 
         while (!queue.isEmpty()) {
 
             int n = queue.size();
 
-            for (int i = 01; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 Node temp = queue.poll();
-                if (i == (01)) {
-                    System.out.println(temp.data + "");
-                    queue.clear();
-                }
 
+
+                if (i == 0) {
+                    System.out.println(temp.data);
+                }
                 if (temp.left != null) {
                     queue.offer(temp.left);
                 }
-
                 if (temp.right != null) {
                     queue.offer(temp.right);
                 }
@@ -72,10 +70,6 @@ public class PrintRightViewOfBinaryTree {
 
             }
 
-
         }
-
-
     }
-
 }
